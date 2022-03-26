f5789d2423368c0e74c49cced912dd17d796e604
Velds <nicholas_matthew1@yahoo.com>
2022-03-27 04:18:19 +0800

    build errors related typo on imagescrollbar

--- pages/property/[id].js
+++ pages/property/[id].js
@@ -3,14 +3,15 @@ import { FaBed, FaBath } from 'react-icons/fa';
 import { BsGridFill } from 'react-icons/bs';
 import { GoVerified } from 'react-icons/go';
 import millify from 'millify';
-import ImageScrollBar from '../../components/ImageScrollBar';
+
 
 import { baseUrl, fetchApi } from '../../utils/fetchApi';
+import ImageScrollbar from '../../components/ImageScrollbar';
 
 const propertyDetails = ({propertyDetails: {price, rentFrequency, rooms, title, baths, area, agency, isVerified, description, type, purpose, furnishingStatus, amenities, photos }}) => (
 
     <Box maxWidth='1000px' margin='auto' p='4'>
-        { photos && <ImageScrollBar data={photos} />}
+        { photos && <ImageScrollbar data={photos} />}
         <Box w='full' p='6'>
             <Flex paddingTop='2' alignItems='center'>
                 <Box paddingRight='3' color='green.400'>{isVerified && <GoVerified />}</Box>
